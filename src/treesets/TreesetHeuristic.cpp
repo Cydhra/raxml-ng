1115bfb570ecc930bed4fa6cdbf030e492584af9
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-11-24 13:24:55 +0100

    implement start tree inference in batches

--- src/treesets/TreesetHeuristic.cpp
+++ src/treesets/TreesetHeuristic.cpp
@@ -1,17 +1,11 @@
 #include "TreesetHeuristic.hpp"
 
-/**
- * Worker thread during the auto-tuning phase
- */
-void run_worker_tuning() {
 
-}
+void TreesetHeuristic::infer_treeset(RaxmlInstance &instance, const Options &opts, CheckpointManager &cm) {
+    auto batch1 = TunedBatch(true, false, 4, 8, 8);
+    batch1.infer_batch(instance, opts);
 
-void TreesetHeuristic::infer_treeset(Options &opts, CheckpointManager &cm) {
-    // step 1: run tree search for singular batches for auto-tuning
-    ParallelContext::init_pthreads_custom(opts, run_worker_tuning,
-                                          recommended_thread_count(), recommended_worker_count());
-    run_worker_tuning();
+    LOG_INFO_TS << "treeset inference complete." << std::endl;
 }
 
 int TreesetHeuristic::recommended_thread_count() {
@@ -24,4 +18,31 @@ int TreesetHeuristic::recommended_worker_count() {
     return 1;
 }
 
+unsigned int TunedBatch::get_batch_size() const {
+    return this->batch_start_trees->size();
+}
 
+
+void TunedBatch::infer_batch(RaxmlInstance &instance, const Options &opts) {
+    LOG_INFO_TS << "Running inference batch [BLO: " << !this->light_spr << ", MO: " << !this->skip_model << ", SPR: " <<
+            this->num_spr << "] with " << this->num_threads << " threads." << std::endl;
+
+    const intVector seeds(this->get_batch_size());
+
+    auto tree_builder = std::bind(thread_start_trees,
+                               std::ref(instance),
+                               std::ref(*this->batch_start_trees),
+                               StartingTree::parsimony,
+                               std::cref(seeds),
+                               0,
+                               false);
+
+    // step 1: infer starting trees
+    ParallelContext::init_pthreads_custom(opts, tree_builder, this->num_threads, this->num_threads);
+    tree_builder();
+    ParallelContext::finalize_threads();
+
+    // TODO store the trees somewhere
+
+
+}
--- src/treesets/TreesetHeuristic.hpp
+++ src/treesets/TreesetHeuristic.hpp
@@ -3,6 +3,14 @@
 
 #include "../Checkpoint.hpp"
 
+// forward declaration of RaxmlInstance
+struct RaxmlInstance;
+
+// forward declaration of thread_start_trees in main.cpp to make it accessible. If the function in main.cpp
+// changes signature, just update this declaration as well.
+void thread_start_trees(RaxmlInstance &instance, TreeList &tree_list, StartingTree st_tree_type,
+                        const intVector &seeds, size_t offset, bool bootstrap);
+
 /**
  * An aggressive stateful local search heuristic for treeset-search. This differs substantially from other topological
  * heuristics because those are stateless (i.e., they do not depend on previous results of the same raxml search).
@@ -12,48 +20,49 @@
  * used for degenerate datasets with extremely low signal.
  * If the likelihood surface has pronounced peaks, this aggressive heuristic will likely not yield optimal results (but
  * this will be detectable by the AU test).
- * 
  */
 class TreesetHeuristic {
 public:
-	explicit TreesetHeuristic() : tuning_phase(true), light_spr(false), skip_model(false), num_spr(0) {}
+    explicit TreesetHeuristic() : num_spr(0) {
+    }
 
-	/**
-	 * Main function for the treeset command. it is different from the thread_main function because it has to
-	 * dynamically adjust load balancing and thread auto-tuning. It is called by master_main instead of starting
-	 * pthreads in `thread_main` if the treeset command is called.
-	 */
-	void infer_treeset(Options &opts, CheckpointManager& cm);
+    /**
+     * Main function for the treeset command. it is different from the thread_main function because it has to
+     * dynamically adjust load balancing and thread auto-tuning. It is called by master_main instead of starting
+     * pthreads in `thread_main` if the treeset command is called.
+     */
+    void infer_treeset(RaxmlInstance &instance, const Options &opts, CheckpointManager &cm);
+
 private:
-	/**
-	 * If true, the heuristic is still tuning parameters,
-	 */
-	bool tuning_phase;
+    /**
+     * If true, the heuristic is still tuning parameters,
+     */
+    bool tuning_phase{true};
 
-	/**
-	 * If true, replace fast SPR rounds with light SPR rounds that do even less BLOs.
-	 */
-	bool light_spr;
+    /**
+     * If true, replace fast SPR rounds with light SPR rounds that do even less BLOs.
+     */
+    bool light_spr{false};
 
-	/**
-	 * If true, skip the first model optimization by reusing model parameters from the previous search.
-	 */
-	bool skip_model;
+    /**
+     * If true, skip the first model optimization by reusing model parameters from the previous search.
+     */
+    bool skip_model{false};
 
-	/**
-	 * How many SPR rounds to perform for each tree search
-	 */
-	unsigned int num_spr;
+    /**
+     * How many SPR rounds to perform for each tree search
+     */
+    unsigned int num_spr;
 
-	/**
-	 * @return the recommended number of threads for workers
-	 */
-	int recommended_thread_count();
+    /**
+     * @return the recommended number of threads for workers
+     */
+    int recommended_thread_count();
 
-	/**
-	 * @return the recommended number of workers per rank
-	 */
-	int recommended_worker_count();
+    /**
+     * @return the recommended number of workers per rank
+     */
+    int recommended_worker_count();
 };
 
 /**
@@ -61,29 +70,49 @@ private:
  */
 class TunedBatch final {
 public:
-	TunedBatch(const bool light_spr, const bool skip_model, const unsigned int num_spr)
-		: light_spr(light_spr),
-		  skip_model(skip_model),
-		  num_spr(num_spr) {
-	}
+    TunedBatch(const bool light_spr, const bool skip_model, const unsigned int num_spr, const unsigned int batch_size, const unsigned int num_threads)
+        : light_spr(light_spr),
+          skip_model(skip_model),
+          num_spr(num_spr),
+          num_threads(num_threads),
+          batch_start_trees(new TreeList(batch_size)) {
+    }
 
-	/**
-	 * If true, replace fast SPR rounds with light SPR rounds that do even less BLOs.
-	 */
-	const bool light_spr;
+    /**
+     * If true, replace fast SPR rounds with light SPR rounds that do even less BLOs.
+     */
+    const bool light_spr;
 
-	/**
-	 * If true, skip the first model optimization by reusing model parameters from the previous search.
-	 */
-	const bool skip_model;
+    /**
+     * If true, skip the first model optimization by reusing model parameters from the previous search.
+     */
+    const bool skip_model;
 
-	/**
-	 * How many SPR rounds to perform for each tree search
-	 */
-	const unsigned int num_spr;
+    /**
+     * How many SPR rounds to perform for each tree search
+     */
+    const unsigned int num_spr;
 
-private:
+    /**
+     * @return the number of trees that are inferred in this batch.
+     */
+    unsigned int get_batch_size() const;
 
+    /**
+     * Using the batch configuration, infer K trees in parallel.
+     */
+    void infer_batch(RaxmlInstance &instance, const Options &opts);
+
+protected:
+    /**
+     * How many threads are used in this batch.
+     */
+    const unsigned int num_threads;
+
+    /**
+     * Starting trees for this inference batch
+     */
+    const shared_ptr<TreeList> batch_start_trees;
 };
 
 #endif //RAXML_TREESETHEURISTIC_HPP_
