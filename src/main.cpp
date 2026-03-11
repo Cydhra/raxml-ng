d9b2952e3832c83c3e82d5625d17b927a884256f
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-03-11 14:42:12 +0100

    change global barrier to local one in au test

--- src/main.cpp
+++ src/main.cpp
@@ -2883,7 +2883,7 @@ void au_test_thread_main(RaxmlInstance& instance, AuTest& tester) {
   const auto slice_start = *worker.start_trees.begin();
 
   tester.run_bootstrap(worker.start_trees.size(), slice_start);
-  ParallelContext::global_barrier();
+  ParallelContext::barrier();
 }
 
 void command_au_test(RaxmlInstance& instance)
@@ -2915,9 +2915,7 @@ void command_au_test(RaxmlInstance& instance)
                                                  std::ref(instance),
                                                  std::ref(*instance.au_test)));
   au_test_thread_main(instance, *instance.au_test);
-  // TODO why do we need to force here? All threads definitely arrive at the barrier, even
-  //  in cases where the finalization call never returns.
-  ParallelContext::finalize_threads(true);
+  ParallelContext::finalize_threads();
 
   // master computes AU values
   instance.au_test->finalize_test_statistics();
