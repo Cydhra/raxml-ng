0cec5efc3f9cfe21ecdb34be68cfcf8e1abffdc3
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-03-26 15:29:45 +0100

    suppress dangling-pointer warning that isn't required

--- src/treesets/TunedBatch.cpp
+++ src/treesets/TunedBatch.cpp
@@ -190,10 +190,14 @@ void TunedBatch::optimize_topology(const Options &opts) {
     // make sure debug outputs are initialized
     unsigned long int total_moves, increasing_moves;
 
-    // escaping doesn't matter because SPR params are only ever used in this function, and are always overwritten
+    // initialize the out-parameters of spr_params with valid addresses
+    // escaping pointers don't matter here because SPR params are only ever used in this function, and are always overwritten
     // ReSharper disable CppDFALocalValueEscapesFunction
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdangling-pointer"
     this->spr_params.total_moves = &total_moves;
     this->spr_params.increasing_moves = &increasing_moves;
+ #pragma GCC diagnostic pop
     // ReSharper restore CppDFALocalValueEscapesFunction
 
     while (this->meta_parameters->num_fast_spr > this->num_fast_spr_performed || this->meta_parameters->num_slow_spr >
