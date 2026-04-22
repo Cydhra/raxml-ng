c47e780d0a5613054004272c9e427b5bdee8af62
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-04-22 12:53:14 +0200

    fix: it was possible to replace bandits with no measurements with the best bandit

--- src/treesets/MultiArmedBandit.hpp
+++ src/treesets/MultiArmedBandit.hpp
@@ -76,7 +76,7 @@ public:
 
         selection_mutex.lock();
         // select next participating bandit
-        // TODO guard against all bandits no longer participating
+        // TODO guard against all bandits no longer participating, while the top-level bandit still does (that shouldnt happen, but maybe verify this)
         do {
             this->bandit_cursor += 1;
             this->bandit_cursor %= this->bandits.size();
@@ -85,43 +85,46 @@ public:
         auto &selected_bandit = this->bandits[this->bandit_cursor];
         auto &best_bandit = this->bandits[this->best_known_bandit];
 
-        if (this->bandit_cursor != this->best_known_bandit && selected_bandit.is_worse_than(
-                best_bandit, iterations_completed)) {
-            LOG_INFO << std::endl << "Switching to best bandit " << best_bandit.get_name() <<
-                    " because its mean expected success ("
-                    << (best_bandit.get_mean_throughput() * 1000.0) <<
-                    " t/s) exceeds the largest reasonable success of "
-                    << selected_bandit.get_name() << " (" << (
-                        selected_bandit.get_upper_confidence(iterations_completed) * 1000.0) << " t/s)." <<
-                    std::endl;
-
-            // check if the selected bandit is so bad that we can just delete it from the round-robin
-            // because this requires both trees to have been selected thrice, this likely only ever excludes parsimony
-            // TODO the exclusion mechanism should be encapsulated a bit better
-            if (selected_bandit.is_hopeless(best_bandit, iterations_completed)) {
-                LOG_INFO << "Excluding bandit " << selected_bandit.get_name() <<
-                        " from algorithm because it is much worse than the others." << std::endl;
-                selected_bandit.participating = false;
-            }
-
-            selection_mutex.unlock();
-            return best_bandit;
-        }
-
-        if (!std::isnan(selected_bandit.get_upper_confidence(iterations_completed))) {
-            if (this->bandit_cursor != this->best_known_bandit) {
-                LOG_INFO << std::endl << "Selecting bandit " << selected_bandit.get_name() <<
-                        " because its largest reasonable success ("
-                        << (selected_bandit.get_upper_confidence(iterations_completed) * 1000.0) <<
-                        " t/s) exceeds the mean expected success of current best bandit "
-                        << best_bandit.get_name() << " (" << (best_bandit.get_mean_throughput() * 1000.0) << " t/s)." <<
-                        std::endl;
-            } else {
-                LOG_INFO << std::endl << "Selecting bandit " << selected_bandit.get_name() << " (mean: " << (
-                    best_bandit.get_mean_throughput() * 1000.0) << " t/s)." << std::endl;
-            }
-        } else {
+        if (selected_bandit.num_samples() == 0) {
             LOG_INFO << std::endl << "Initial estimation of " << selected_bandit.get_name() << "." << std::endl;
+        } else {
+            if (this->bandit_cursor != this->best_known_bandit && selected_bandit.is_worse_than(
+                    best_bandit, iterations_completed)) {
+                LOG_INFO << std::endl << "Switching to best bandit " << best_bandit.get_name() <<
+                        " because its mean expected success ("
+                        << (best_bandit.get_mean_throughput() * 1000.0) <<
+                        " t/s) exceeds the largest reasonable success of "
+                        << selected_bandit.get_name() << " (" << (
+                            selected_bandit.get_upper_confidence(iterations_completed) * 1000.0) << " t/s)." <<
+                        std::endl;
+
+                // check if the selected bandit is so bad that we can just delete it from the round-robin
+                // because this requires both trees to have been selected thrice, this likely only ever excludes parsimony
+                // TODO the exclusion mechanism should be encapsulated a bit better
+                if (selected_bandit.is_hopeless(best_bandit, iterations_completed)) {
+                    LOG_INFO << "Excluding bandit " << selected_bandit.get_name() <<
+                            " from algorithm because it is much worse than the others." << std::endl;
+                    selected_bandit.participating = false;
+                }
+
+                selection_mutex.unlock();
+                return best_bandit;
+            }
+
+            if (!std::isnan(selected_bandit.get_upper_confidence(iterations_completed))) {
+                if (this->bandit_cursor != this->best_known_bandit) {
+                    LOG_INFO << std::endl << "Selecting bandit " << selected_bandit.get_name() <<
+                            " because its largest reasonable success ("
+                            << (selected_bandit.get_upper_confidence(iterations_completed) * 1000.0) <<
+                            " t/s) exceeds the mean expected success of current best bandit "
+                            << best_bandit.get_name() << " (" << (best_bandit.get_mean_throughput() * 1000.0) <<
+                            " t/s)." <<
+                            std::endl;
+                } else {
+                    LOG_INFO << std::endl << "Selecting bandit " << selected_bandit.get_name() << " (mean: " << (
+                        best_bandit.get_mean_throughput() * 1000.0) << " t/s)." << std::endl;
+                }
+            }
         }
 
         selection_mutex.unlock();
@@ -158,7 +161,7 @@ public:
     }
 
     bool has_bandit(std::string name) const {
-        for (auto &bandit : this->bandits) {
+        for (auto &bandit: this->bandits) {
             if (bandit.name == name) {
                 return true;
             }
