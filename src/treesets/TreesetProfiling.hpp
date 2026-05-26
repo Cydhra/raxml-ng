16abcc11aaebbe9a60d6721d10ef4d5a12306966
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-05-26 17:21:33 +0200

    remove debug lines

--- src/treesets/TreesetProfiling.hpp
+++ src/treesets/TreesetProfiling.hpp
@@ -65,12 +65,9 @@ class CountingProfiler : public Profiler {
 public:
     void finish_measurement(const TunedBatch &batch, const InferencePhase &phase) override {
         if (std::holds_alternative<CompleteInference>(phase)) {
-            LOG_INFO << ">>> measuring batch [" << batch.get_name() << "]" << std::endl;
             std::lock_guard lock(this->m);
             const auto count = count_profile(batch);
-            LOG_INFO << ">>> samples has " << samples.size() << " elements before adding the measurement (" << count << ")" << std::endl;
             samples.push_back(count);
-            LOG_INFO << ">>> samples has " << samples.size() << " elements after adding the measurement." << std::endl;
         }
     }
 
@@ -84,7 +81,6 @@ public:
 
         double average = 0.0;
         for (const auto count: measurements) {
-            printf(">>>> SAMPLE: %d\n", count);
             average += static_cast<double>(count);
         }
         average /= static_cast<double>(measurements.size());
