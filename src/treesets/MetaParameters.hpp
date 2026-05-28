d90ceda80c322179da8b1604eee7cb63221aae85
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-05-28 11:24:07 +0200

    make MetaParameters a struct to clearly communicate its data-only purpose

--- src/treesets/MetaParameters.hpp
+++ src/treesets/MetaParameters.hpp
@@ -6,66 +6,67 @@
 #include <string>
 
 /**
- * Class that holds the meta-parameters of self-tuning tree inference. The parameters are held by a bandit and can
+ * Holds the meta-parameters of self-tuning tree inference. The parameters are held by a bandit and can
  * be applied to TunedBatches to configure the tree inference.
  */
-class MetaParameters {
-public:
+struct MetaParameters {
     /**
      * Number of top topologies to keep during SPR rounds. If lower, the SPR rounds perform substantially less
      * branch-length optimizations. On datasets where most trees are plausible, this saves time because the K top
      * topologies are probably equally likely anyway.
      */
-    const unsigned int keep_top_k_topol;
+    unsigned int keep_top_k_topol;
 
     /**
      * If true, skip the first model optimization by reusing model parameters from a previous search.
      */
-    const bool skip_model;
+    bool skip_model;
 
     /**
      * How many fast SPR rounds to perform for each tree search
      */
-    const unsigned int num_fast_spr;
+    unsigned int num_fast_spr;
 
     /**
      * How many slow SPR rounds to perform for each tree search
      */
-    const unsigned int num_slow_spr;
+    unsigned int num_slow_spr;
 
     /**
      * If true, skip all model optimization and simply accept parsimony starting trees as the final resulting topology.
      */
-    const bool accept_starting_trees;
+    bool accept_starting_trees;
 
     /**
      * If true, the 0.1 model optimization is done instantly to commit the tree and model to the current local minimum.
      * This helps with datasets with discordant signal to commit to a random signal instantly, instead of searching
      * a global minimum.
      */
-    const bool early_commit;
+    bool early_commit;
 
     /**
      * Maximum SPR radius
      */
-    const unsigned int max_adaptive_radius;
+    unsigned int max_adaptive_radius;
 
     /**
      * Perform an NNI round before the SPR rounds
      */
-    const bool nni_round;
+    bool nni_round;
 
     /**
      * If true, constrain the tree to ignore parts of the tree that are probably unresolvable.
      */
-    const bool constrain;
+    bool constrain;
 
     /**
      * If set, override the model with a model built from this model string.
      */
-    const std::optional<std::string> model_override;
+    std::optional<std::string> model_override;
 
-    MetaParameters(const unsigned int keep_top_k_topol, const bool skip_model, const unsigned int num_fast_spr,
+    MetaParameters(const unsigned int keep_top_k_topol,
+                   const bool skip_model,
+                   const unsigned int num_fast_spr,
                    const unsigned int num_slow_spr,
                    const bool accept_starting_trees = false,
                    const bool early_commit = false,
