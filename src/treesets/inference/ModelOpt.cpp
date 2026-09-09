6f4596dd2af7271ab5e033e17ec9a5dd7bfc0b0e
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-09-09 15:56:41 +0200

    fix: model optimization did not iterate

--- src/treesets/inference/ModelOpt.cpp
+++ src/treesets/inference/ModelOpt.cpp
@@ -1,21 +1,37 @@
 #include "ModelOpt.hpp"
 #include "../batch/SharedBatchResources.hpp"
 
-void ModelOpt::do_optimize(std::optional<TreeInfo> &tree, const unsigned int tree_id, const Options&, const TaskGroup &context, SharedBatchResources &, const unsigned int worker_id, const unsigned int thread_id) {
+void ModelOpt::do_optimize(std::optional<TreeInfo> &tree, const unsigned int tree_id, const Options &,
+                           const TaskGroup &context, SharedBatchResources &, const unsigned int worker_id,
+                           const unsigned int thread_id) {
     if (model && branches) {
         if (context.is_group_leader(worker_id, thread_id) && tree_id == 0) {
             LOG_INFO_TS << this->batch_name << ": Optimizing all params (eps: " << epsilon << ")" << std::endl;
         }
 
-        // run all parameters optimization
-        tree->optimize_params(CORAX_OPT_PARAM_ALL, epsilon);
+        double loglh = tree->optimize_params(CORAX_OPT_PARAM_ALL & ~CORAX_OPT_PARAM_BRANCHES_ITERATIVE, epsilon);
+        double new_loglh = tree->optimize_params(CORAX_OPT_PARAM_BRANCHES_ITERATIVE, epsilon);
+
+        while (new_loglh - loglh > epsilon) {
+            loglh = new_loglh;
+            new_loglh = tree->optimize_params(CORAX_OPT_PARAM_ALL, epsilon);
+        }
+
+        if (context.is_group_leader(worker_id, thread_id) && tree_id == 0) {
+            LOG_INFO_TS << this->batch_name << ": after: " << new_loglh << std::endl;
+        }
     } else if (model) {
         if (context.is_group_leader(worker_id, thread_id) && tree_id == 0) {
             LOG_INFO_TS << this->batch_name << ": Optimizing model (eps: " << epsilon << ")" << std::endl;
         }
 
-        // run model optimization
-        tree->optimize_model(epsilon);
+        double loglh = tree->loglh(true);
+        double new_loglh = tree->optimize_model(epsilon);
+
+        while (new_loglh - loglh > epsilon) {
+            loglh = new_loglh;
+            new_loglh = tree->optimize_model(epsilon);
+        }
     } else if (branches) {
         if (context.is_group_leader(worker_id, thread_id) && tree_id == 0) {
             LOG_INFO_TS << this->batch_name << ": Optimizing branches (eps: " << epsilon << ")" << std::endl;
